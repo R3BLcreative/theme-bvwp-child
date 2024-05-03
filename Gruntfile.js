c9aa9f278bb7963d8151053c981b39b3fa5a0bdf
James Cook <jcook@r3blcreative.com>
2024-05-03 15:20:22 -0500

    INIT

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function (grunt) {
 					{ src: './infinite/README.md', dest: '<%= pkg.name %>/' },
 					{ src: './infinite/index.php', dest: '<%= pkg.name %>/' },
 					{ src: './infinite/LICENSE.txt', dest: '<%= pkg.name %>/' },
-					{ src: './includes/*.php', dest: '<%= pkg.name %>/' },
+					{ src: './plugin-update-checker/**', dest: '<%= pkg.name %>/' },
 					{ src: './style.css', dest: '<%= pkg.name %>/' },
 					{ src: './functions.php', dest: '<%= pkg.name %>/' },
 					{ src: './screenshot.png', dest: '<%= pkg.name %>/' },
@@ -50,33 +50,34 @@ module.exports = function (grunt) {
 		},
 	});
 
-	grunt.registerTask('manifest', function (key, value) {
-		// Get config package.json
-		var pkg = grunt.config.get('pkg');
+	// grunt.registerTask('manifest', function (key, value) {
+	// 	// Get config package.json
+	// 	var pkg = grunt.config.get('pkg');
 
-		// Set changing props & default props
-		var website = 'https://r3blcreative.com';
-		var rootPath = website + '/r3bl-updates/themes/' + pkg['name'] + '/';
+	// 	// Set changing props & default props
+	// 	var website = 'https://r3blcreative.com';
+	// 	var rootPath = website + '/r3bl-updates/themes/' + pkg['name'] + '/';
 
-		var wp = {
-			version: pkg['version'],
-			requires: '6.4.3',
-			requires_php: '8.0.0',
-			download_url: rootPath + pkg['name'] + '.zip?v=' + pkg['version'],
-			details_url: rootPath + 'changelog.html',
-		};
+	// 	var wp = {
+	// 		version: pkg['version'],
+	// 		requires: '6.4.3',
+	// 		requires_php: '8.0.0',
+	// 		download_url: rootPath + pkg['name'] + '.zip?v=' + pkg['version'],
+	// 		details_url: rootPath + 'changelog.html',
+	// 	};
 
-		// Path to write/update file
-		var infoJsonFile = './dist/info.json';
+	// 	// Path to write/update file
+	// 	var infoJsonFile = './dist/info.json';
 
-		// Write/update file
-		grunt.file.write(infoJsonFile, JSON.stringify(wp));
-	});
+	// 	// Write/update file
+	// 	grunt.file.write(infoJsonFile, JSON.stringify(wp));
+	// });
 
 	// Load grunt plugins
 	grunt.loadNpmTasks('grunt-contrib-compress');
 	grunt.loadNpmTasks('grunt-string-replace');
 
 	// Register tasks
-	grunt.registerTask('default', ['string-replace', 'compress', 'manifest']);
+	// grunt.registerTask('default', ['string-replace', 'compress', 'manifest']);
+	grunt.registerTask('default', ['string-replace', 'compress']);
 };
